52cd283987ee8ef26fd52864b2a4d893107f0e64
Wesley Harris <mharri01@cs.nmt.edu>
2016-09-20 00:27:20 -0600

    debug construct curve; need to add a circular quadrant check

--- tao2.c
+++ tao2.c
@@ -19,16 +19,14 @@ struct vector_t {
 };
 
 struct function_t {
-    struct point_t point;
-    double x;
-    double y;
+    struct point_t point1;
+    struct point_t point2;
     double m;
-    double b;
 };
 
 struct triangle_t {
     char name[3];
-    char *points;
+    char points[SIZE - 1];
     struct function_t f1;
     struct function_t f2;
     struct function_t f3;
@@ -133,21 +131,23 @@ double shortest_path(struct point_t *point, int size)
 int **construct_curve(struct point_t *point, int size)
 {
     struct point_t center;
+    struct point_t prev;
+    struct vector_t *position = malloc(sizeof(struct vector_t) * size);
     double *distance = malloc(sizeof(double) * size);
     double range[2] = {0};
     double shortest = DBL_MAX;
     double longest = 0;
     double tao = 0;
     double division_constant = 0;
+    double sum_x = 0;
+    double sum_y = 0;
     int **curve = malloc(sizeof(int *) * size);
     int **neighbors = malloc(sizeof(int *) * size);
-    int shortest_path[2];
+    int shortest_curvature[2];
     int i = 0;
     int j = 0;
     int k = 0;
     int tmp = 0;
-    int sum_x = 0;
-    int sum_y = 0;
     int division_number = 0;
     /* calculate average point */
     for(i = 0; i <= size; i++) {
@@ -156,6 +156,18 @@ int **construct_curve(struct point_t *point, int size)
     }
     center.x = sum_x / size;
     center.y = sum_y / size;
+    /* calculate position vectors */
+    for(i = 0; i <= size; i++) {
+        position[i].point[0].x = center.x;
+        position[i].point[0].y = center.y;
+        position[i].point[0].index = SIZE;
+        position[i].point[1].x = point[i].x;
+        position[i].point[1].y = point[i].y;
+        position[i].point[1].index = SIZE;
+        position[i].i = point[i].x - center.x;
+        position[i].j = point[i].y - center.y;
+        position[i].length = length_v(position[i]);
+    }
     /* find shortest and longest distances */
     for(i = 0; i <= size; i++) {
         distance[i] = distance_p(center, point[i]);
@@ -181,15 +193,14 @@ int **construct_curve(struct point_t *point, int size)
         }
         for(j = 0; j <= size; j++) {
             /* range[0] < distance[j] < range[1] */
-            if((distance[j] >= range[0]) && (distance[j] <= range[1])) {
+            if((distance[j] >= (range[0] - 0.01)) && (distance[j] <= (range[1] + 0.01))) {
                 curve[i][j] = j;
-                //k++;
             }
         }
         range[0] = range[1];
         k = 0;
     }
-    /* printing for debug */
+    /* printing for debug
     for(i = 0; i < division_number; i++) {
         printf("curve[%d]: ", i);
         for(j = 0; j <= size; j++) {
@@ -198,7 +209,7 @@ int **construct_curve(struct point_t *point, int size)
             }
         }
         printf("\n");
-    }//*/
+    }*/
     /* fills neighbors in a 2D array of length (size x 2)
        with the indices of the two neighbors (index of SIZE means
        a neighbor wasn't found)
@@ -212,56 +223,59 @@ int **construct_curve(struct point_t *point, int size)
     for(i = 0; i < division_number; i++) {
         /* calculate distances between all paths within curve[i] */
         for(j = 0; j <= size; j++) {
-            /* initializes shortest_path[] array to a non-existant
+            /* initializes shortest_curvature[] array to a non-existant
                node */
-            shortest_path[0] = SIZE;
-            shortest_path[1] = SIZE;
+            shortest_curvature[0] = SIZE;
+            shortest_curvature[1] = SIZE;
             /* first find a starting point (index j) */
             if(curve[i][j] != SIZE) {
                 /* then compare distances between all the other
-                   points (index k) */
+                   points (index k)
+                */
+                /* initializing previous point for j */
+                prev.x = point[j].x + (position[j].i / position[j].length);
+                prev.y = point[j].y + (position[j].j / position[j].length);
+                prev.index = SIZE;
                 for(k = 0; k <= size; k++) {
                     if((curve[i][k] != SIZE) && (k != j)) {
                         /* stores the two shortest paths
-                           -- checks to see if it is the first path
-                           -- or then the second path
-                           -- stores paths if the current distance is
-                              less than the distance to the previous
-                              shortest path */
-                        if(shortest_path[0] == 14) {
-                            shortest_path[0] = k;
+                           -- stores paths if the current curvature is
+                              less than the curvature of the last
+                              point */
+                        if(shortest_curvature[0] == SIZE) {
+                            shortest_curvature[0] = k;
                         }
-                        else if(shortest_path[1] == 14) {
-                            shortest_path[1] = k;
+                        else if(shortest_curvature[1] == SIZE) {
+                            shortest_curvature[1] = k;
                             /* keeps the smallest value in index 0 */
-                            if (shortest_path[1] < shortest_path[0]) {
-                                tmp = shortest_path[0];
-                                shortest_path[0] = shortest_path[1];
-                                shortest_path[1] = tmp;
+                            if (calculate_curvature(prev, point[j], point[shortest_curvature[1]]) <= calculate_curvature(prev, point[j], point[shortest_curvature[0]])) {
+                                tmp = shortest_curvature[0];
+                                shortest_curvature[0] = shortest_curvature[1];
+                                shortest_curvature[1] = tmp;
                             }
                         }
-                        else if(distance_p(point[j], point[k]) <= distance_p(point[j], point[shortest_path[1]])) {
+                        else if(calculate_curvature(prev, point[j], point[k]) <= calculate_curvature(prev, point[j], point[shortest_curvature[1]])) {
                             /* stores new shortest path */
-                            shortest_path[1] = k;
+                            shortest_curvature[1] = k;
                             /* keeps the smallest value in index 0 */
-                            if (shortest_path[1] < shortest_path[0]) {
-                                tmp = shortest_path[0];
-                                shortest_path[0] = shortest_path[1];
-                                shortest_path[1] = tmp;
+                            if (calculate_curvature(prev, point[j], point[shortest_curvature[1]]) <= calculate_curvature(prev, point[j], point[shortest_curvature[0]])) {
+                                tmp = shortest_curvature[0];
+                                shortest_curvature[0] = shortest_curvature[1];
+                                shortest_curvature[1] = tmp;
                             }
-                        }
-                        else if(distance_p(point[j], point[k]) <= distance_p(point[j], point[shortest_path[1]])) {
-                            /* stores new shortest path */
-                            shortest_path[1] = k;
                         }
                     }
                 }
-                neighbors[j][0] = shortest_path[0];
-                neighbors[j][1] = shortest_path[1];
+                /* printing for debug
+                printf("curv[%d] = %0.2lf, : curv[%d] = %0.2lf\n", shortest_curvature[0], calculate_curvature(prev, point[j], point[shortest_curvature[0]]), shortest_curvature[1], calculate_curvature(prev, point[j], point[shortest_curvature[1]])); */
+                /* sets neighbors of node i */
+                neighbors[j][0] = shortest_curvature[0];
+                neighbors[j][1] = shortest_curvature[1];
             }
         }
     }
     /* printing for debug */
+    printf("NEIGHBORS ARRAY:\n");
     for(i = 0; i <= size; i++) {
         printf("node %d: %d, %d\n", i, neighbors[i][0], neighbors[i][1]);
     }
@@ -275,17 +289,11 @@ struct triangle_t *construct_triangles(struct point_t **curve, int size,int divi
     return triangle;
 }
 
-/* calculates theta given two vectors */
-double calculate_theta(struct vector_t V1, struct vector_t V2)
-{
-    return acos(dot_product(V1, V2)/(V1.length * V2.length));
-}
-
 /* calculates curvature given the previous, current, and next points */
 double calculate_curvature(struct point_t prev, struct point_t curr, struct point_t next)
 {
     /* initializing point data
-       -- initializing vector V <prev, curr> */
+       -- initializing vector V <prev, curr>
     struct vector_t V1;
     V1.point[0].x = prev.x;
     V1.point[0].y = prev.y;
@@ -295,18 +303,18 @@ double calculate_curvature(struct point_t prev, struct point_t curr, struct poin
     V1.point[1].index = curr.index;
     V1.i = V1.point[1].x - V1.point[0].x;
     V1.j = V1.point[1].y - V1.point[0].y;
-    V1.length = length_v(V1);
+    V1.length = length_v(V1);*/
     /* -- initializing vector T1 */
     struct vector_t T1;
     T1.point[0].x = curr.x;
     T1.point[0].y = curr.y;
     T1.point[0].index = curr.index;
-    T1.point[1].x = (V1.i / V1.length) + V1.i;
-    T1.point[1].y = (V1.j / V1.length) + V1.j;
-    T1.point[1].index = 14;
+    T1.point[1].x = prev.x;
+    T1.point[1].y = prev.y;
+    T1.point[1].index = prev.index;
     T1.i = T1.point[1].x - T1.point[0].x;
     T1.j = T1.point[1].y - T1.point[0].y;
-    T1.length = 1;
+    T1.length = length_v(T1);
     /* initializing vector V2, <curr, next> */
     struct vector_t V2;
     V2.point[0].x = curr.x;
@@ -315,21 +323,40 @@ double calculate_curvature(struct point_t prev, struct point_t curr, struct poin
     V2.point[1].x = next.x;
     V2.point[1].y = next.y;
     V2.point[1].index = next.index;
-    V2.i = V1.point[1].x - V1.point[0].x;
-    V2.j = V1.point[1].y - V1.point[0].y;
+    V2.i = V2.point[1].x - V2.point[0].x;
+    V2.j = V2.point[1].y - V2.point[0].y;
     V2.length = length_v(V2);
     /* -- initializing vector T2 */
     struct vector_t T2;
     T2.point[0].x = curr.x;
     T2.point[0].y = curr.y;
     T2.point[0].index = curr.index;
-    T2.point[1].x = (V2.i / V2.length) + V2.i;
-    T2.point[1].y = (V2.j / V2.length) + V2.j;
-    T2.point[1].index = 14;
+    T2.point[1].x = (V2.i / V2.length) + T2.point[0].x;
+    T2.point[1].y = (V2.j / V2.length) + T2.point[0].y;
+    T2.point[1].index = SIZE;
     T2.i = T2.point[1].x - T2.point[0].x;
     T2.j = T2.point[1].y - T2.point[0].y;
-    T2.length = 1;
-    return (length_v(subtract_v(T1, T2)) / calculate_theta(T1, T2));
+    T2.length = length_v(T2);
+    //return (length_v(subtract_v(T1, T2)) / calculate_theta(T1, T2));
+    return (calculate_theta(T1, T2));
+}
+
+/* calculates theta given two vectors */
+double calculate_theta(struct vector_t V1, struct vector_t V2)
+{
+    //printf("dot_product<%0.2lf,%0.2lf>,<%0.2lf,%0.2lf> = %0.2lf\n", V1.i, V1.j, V2.i, V2.j, dot_product(V1, V2));
+    //printf("length<%0.2lf,%0.2lf> = %0.2lf,length<%0.2lf,%0.2lf> = %0.2lf\n", V1.i, V1.j, V1.length, V2.i, V2.j, V2.length);
+    return acos(dot_product(V1, V2)/(V1.length * V2.length));
+}
+
+/* calculates dot product of two vectors */
+double dot_product(struct vector_t start, struct vector_t end)
+{
+    double i_1 = start.i;
+    double j_1 = start.j;
+    double i_2 = end.i;
+    double j_2 = end.j;
+    return ((i_2 * i_1) + (j_2 * j_1));
 }
 
 /* calculates tao_distance given theta, length and curvature */
@@ -378,14 +405,4 @@ struct vector_t subtract_v(struct vector_t V2, struct vector_t V1)
     S.j = S.point[1].y - S.point[0].y;
     S.length = length_v(S);
     return S;
-}
-
-/* calculates dot product of two vectors */
-double dot_product(struct vector_t start, struct vector_t end)
-{
-    double i_1 = start.i;
-    double j_1 = start.j;
-    double i_2 = end.i;
-    double j_2 = end.j;
-    return ((i_2 * i_1) + (j_2 * j_1));
 }
